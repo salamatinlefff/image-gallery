c9b89d20ccf0e65f8bc7f21a0addc00561d4bcd8
Lev <salamatinlefff@gmail.com>
2022-02-14 17:21:20 +0400

    feat: add random query onload

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,7 @@
-import { getData, search as getSearch } from './services.js';
+import { randomPage, randomQuery, search as getSearch } from './services.js';
 import search, { inputFocus } from './search.js';
 import renderCards from './renderCards.js';
 
 inputFocus();
 search();
-renderCards(await getSearch('random'));
+renderCards(await getSearch(randomQuery(), randomPage()));
--- assets/scripts/services.js
+++ assets/scripts/services.js
@@ -1,5 +1,22 @@
 const API_KEY = '-2rHtX23l_bJmSZ2xwpmt1rGksEHfeW48dmuEJjsYZU';
-const BASE_URL = 'https://api.unsplash.com/'
+const BASE_URL = 'https://api.unsplash.com/';
+
+const requests = ['kitten', 'dogs', 'fox','cars', 'sea', 'summer', 'winter', 'autumn', 'spring', 'girls', 'computer', 'random', 'technologies', 'science', 'smile', 'city'];
+
+const randomInt = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export const randomQuery = () => {
+  const max = requests.length - 1;
+
+  return requests[randomInt(max)];
+}
+
+export const randomPage = () => {
+  const max = 15;
+  return randomInt(max);
+}
 
 export const getData = async(url) => {
   const res = await fetch(url);
